0b8521e1e9d64f19bfc024cead5bd92f3fa211cb
Markus Feyh <markus.feyh@gmail.com>
2018-08-01 13:23:26 -0700

    Add stop

--- src/lock_statem.erl
+++ src/lock_statem.erl
@@ -1,1 +1,15 @@
 -module(lock_statem).
+-behavior(gen_statem).
+
+-export([start_link/0, callback_mode/0, init/1, stop/0]).
+
+
+start_link() ->
+    gen_statem:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+callback_mode() -> handle_event_function.
+
+init([]) -> {ok, locked, state}.
+
+stop() ->
+    gen_statem:stop(?MODULE).
