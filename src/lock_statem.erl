ed22011f5be2a2301be11a5f363d768267d7adf5
Markus Feyh <markus.feyh@gmail.com>
2018-08-01 13:54:30 -0700

    Make unlock_test pass

--- src/lock_statem.erl
+++ src/lock_statem.erl
@@ -5,13 +5,14 @@
 
 -export([get_state/0, get_count/0, press_digit/1]).
 
+-define(EXPECTED_CODE, "1234").
 
 start_link() ->
     gen_statem:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 callback_mode() -> handle_event_function.
 
-init([]) -> {ok, locked, #{count => 0}}.
+init([]) -> {ok, locked, #{digits => [], count => 0}}.
 
 get_state() ->
     gen_statem:call(?MODULE, get_state).
@@ -22,9 +23,15 @@ get_count() ->
 press_digit(Digit) ->
     gen_statem:call(?MODULE, {press_digit, Digit}).
 
-handle_event({call, From}, {press_digit, _Digit}, locked, #{count := Count} = Data) ->
-    NewData = Data#{count => Count + 1},
-    {next_state, locked, NewData, {reply, From, locked}};
+handle_event({call, From}, {press_digit, Digit}, locked, #{digits := Digits, count := Count} = Data) ->
+    Entered_Code = Digits ++ Digit,
+    case Entered_Code =:= ?EXPECTED_CODE of
+        true ->
+            {next_state, unlocked, #{digits => [], count => 0}, [{reply, From, unlocked}]};
+        false ->
+            NewData = Data#{digits => Entered_Code, count => Count + 1},
+            {next_state, locked, NewData, [{reply, From, locked}]}
+    end;
 handle_event({call, From}, get_count, State, #{count := Count} = Data) ->
     {next_state, State, Data, {reply, From, Count}};
 handle_event({call, From}, get_state, State, Data) ->
